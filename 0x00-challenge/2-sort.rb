6d933d6d5202d827bae6873eca1750d0daa6c6ea
medazza <midazza27@gmail.com>
2023-12-19 13:52:14 +0100

    fix task 2 error

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,10 +17,10 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
+        if result[i] <= i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
