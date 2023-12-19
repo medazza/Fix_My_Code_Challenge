034cfb3ad2fa9e6eb252e93107bf299b4990f707
medazza <midazza27@gmail.com>
2023-12-19 15:04:15 +0100

    task 3 fix errror update

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     if user_1.password == u_pwd:
         print("User.password should be hashed")
 
-    if user_2.password is not None:
+    if user_2.password is None:
         print("User.password should be None by default")
 
     user_2.password = None
